1bf56f585daa942b8affe9313ce78d5e5e0c626f
Rayan Perotti-Valle <perotti.rayan@gmail.com>
2024-12-10 17:17:40 +0100

    ttt

--- Assets/Components/LevelEditor/NewLevelToLoad.cs
+++ Assets/Components/LevelEditor/NewLevelToLoad.cs
@@ -3,4 +3,5 @@ using UnityEngine;
 public class NewLevelToLoad : MonoBehaviour {
 	// Advice: FYFY component aims to contain only public members (according to Entity-Component-System paradigm).
 	public string levelKey;
+	
 }
